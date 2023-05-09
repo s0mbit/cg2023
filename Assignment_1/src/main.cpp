17d42af10d70a5530265122f8dbb7ff6b15df51f
Sebastian Klement <Sebastian.Klement@HS-Augsburg.de>
2023-05-08 20:48:48 -0700

    finally i can debug fuck this shit

--- Assignment_1/src/main.cpp
+++ Assignment_1/src/main.cpp
@@ -5,12 +5,15 @@
 #include <iostream>
 #include <numeric>
 #include <vector>
-////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////// 
+using namespace std;
 
-const std::string root_path = DATA_DIR;
+const string root_path = "/home/sk/CG-Summer-2023/Assignment_1/data";
 
-typedef std::complex<double> Point;
-typedef std::vector<Point> Polygon;
+
+
+typedef complex<double> Point;
+typedef vector<Point> Polygon;
 
 double inline det(const Point &u, const Point &v)
 {
@@ -39,64 +42,138 @@ bool is_inside(const Polygon &poly, const Point &query)
 
 ////////////////////////////////////////////////////////////////////////////////
 
+double cross_product(const Point &p1, const Point &p2)
+{
+    return p1.real() * p2.imag() - p2.real() * p1.imag();
+}
+
+double distance_squared(const Point &p1, const Point &p2)
+{
+    double dx = p1.real() - p2.real();
+    double dy = p1.imag() - p2.imag();
+    return dx * dx + dy * dy;
+}
+
 struct Compare
 {
     Point p0; // Leftmost point of the poly
     bool operator()(const Point &p1, const Point &p2)
     {
-        // TODO
-        return true;
+        auto cross = cross_product(p1 - p0, p2 - p0);
+        if (cross == 0)
+        {
+            auto d1 = distance_squared(p1, p0);
+            auto d2 = distance_squared(p2, p0);
+            return d1 < d2;
+        }
+        return cross > 0;
     }
 };
 
-bool inline salientAngle(Point &a, Point &b, Point &c)
+bool inline salientAngle(const Point &a, const Point &b, const Point &c)
 {
-    // TODO
-    return false;
+    auto cross = cross_product(b - a, c - a);
+    return cross > 0;
 }
 
-Polygon convex_hull(std::vector<Point> &points)
+Polygon convex_hull(vector<Point> &points)
 {
     Compare order;
-    // TODO
-    order.p0 = Point(0, 0);
-    std::sort(points.begin(), points.end(), order);
+    order.p0 = *min_element(points.begin(), points.end(), [](const Point &p1, const Point &p2) { return p1.real() < p2.real(); });
+    sort(points.begin(), points.end(), order);
+
     Polygon hull;
-    // TODO
-    // use salientAngle(a, b, c) here
+    hull.push_back(points[0]);
+    hull.push_back(points[1]);
+
+    for (size_t i = 2; i < points.size(); ++i)
+    {
+        while (hull.size() >= 2 && !salientAngle(*(hull.rbegin() + 1), hull.back(), points[i]))
+        {
+            hull.pop_back();
+        }
+        hull.push_back(points[i]);
+    }
+
     return hull;
 }
 
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
 
-std::vector<Point> load_xyz(const std::string &filename)
+vector<Point> load_xyz(const string &filename)
 {
-    std::vector<Point> points;
-    std::ifstream in(filename);
+    vector<Point> points;
+    ifstream in(filename);
     // TODO
+    
+    size_t num_points;
+    in >> num_points;
+    double x, y, z;
+    for (size_t i = 0; i < num_points; ++i) {
+        in >> x >> y >> z;
+
+        //cout << x << endl;
+
+        points.push_back(Point(x, y));
+    }
     return points;
 }
 
-void save_xyz(const std::string &filename, const std::vector<Point> &points)
+void save_xyz(const string &filename, const vector<Point> &points)
 {
-    // TODO
+    ofstream out(filename);
+
+    for (const auto &p : points)
+    {
+        out << p.real() << ' ' << p.imag() << " 0\n";
+    }
+    out.close();
 }
 
-Polygon load_obj(const std::string &filename)
-{
-    std::ifstream in(filename);
-    // TODO
-    return {};
+Polygon load_obj(const string& filename) {
+    ifstream in(filename);
+    if (!in.is_open()) {
+        throw runtime_error("failed to open file " + filename);
+    }
+
+    Polygon vertices;
+    string line;
+    while (getline(in, line)) {
+        if (line.empty() || line[0] == '#') {
+            continue;
+        }
+
+        istringstream iss(line);
+        string type;
+        iss >> type;
+
+        if (type == "v") {
+            double x, y, z;
+            iss >> x >> y >> z;
+            vertices.emplace_back(x, y);
+        } else if (type == "f") {
+            string index_str;
+            while (iss >> index_str) {
+                vertices.push_back(vertices[stoi(index_str) - 1]);
+            }
+        }
+    }
+
+    return vertices;
 }
 
-void save_obj(const std::string &filename, Polygon &poly)
+
+void save_obj(const string &filename, Polygon &poly)
 {
-    std::ofstream out(filename);
+    ofstream out(filename);
     if (!out.is_open())
     {
-        throw std::runtime_error("failed to open file " + filename);
+        throw runtime_error("failed to open file " + filename);
     }
-    out << std::fixed;
+    out << fixed;
     for (const auto &v : poly)
     {
         out << "v " << v.real() << ' ' << v.imag() << " 0\n";
@@ -105,24 +182,28 @@ void save_obj(const std::string &filename, Polygon &poly)
     {
         out << "l " << i + 1 << ' ' << 1 + (i + 1) % poly.size() << "\n";
     }
-    out << std::endl;
+    out << endl;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char *argv[])
 {
-    const std::string points_path = root_path + "/points.xyz";
-    const std::string poly_path = root_path + "/polygon.obj";
+    cout << "starting.." << endl;
+    
 
-    std::vector<Point> points = load_xyz(points_path);
+    const string points_path = root_path + "/points.xyz";
+    const string poly_path = root_path + "/polygon.obj";
+
+    vector<Point> points = load_xyz(points_path);
 
     ////////////////////////////////////////////////////////////////////////////////
     //Point in polygon
     Polygon poly = load_obj(poly_path);
-    std::vector<Point> result;
+    vector<Point> result;
     for (size_t i = 0; i < points.size(); ++i)
     {
+        cout << "inside Point in Polygon loop: " << i << endl;
         if (is_inside(poly, points[i]))
         {
             result.push_back(points[i]);
@@ -132,8 +213,14 @@ int main(int argc, char *argv[])
 
     ////////////////////////////////////////////////////////////////////////////////
     //Convex hull
+    cout << "convex hull" << endl;
     Polygon hull = convex_hull(points);
+
+    cout << "finished hull" << endl;
+
     save_obj("output.obj", hull);
+
+    cout << "finished obj save" << endl;
 
     return 0;
 }
