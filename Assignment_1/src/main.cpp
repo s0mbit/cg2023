9ad943518196c6e61b3ea2a3e73fea91f738a02e
Sebastian Klement <Sebastian.Klement@HS-Augsburg.de>
2023-05-15 10:48:00 -0700

    update

--- Assignment_1/src/main.cpp
+++ Assignment_1/src/main.cpp
@@ -17,41 +17,77 @@ typedef vector<Point> Polygon;
 
 double inline det(const Point &u, const Point &v)
 {
-    // TODO
-    return u.real() * u.imag() - v.real() * v.imag();
+    return (u.real() * v.imag() - u.imag() * v.real());
 }
 
 // Return true iff [a,b] intersects [c,d], and store the intersection in ans
 bool intersect_segment(const Point &a, const Point &b, const Point &c, const Point &d, Point &ans)
 {
-    // TODO
+    // Compute vectors
+    Point ab = b - a;
+    Point cd = d - c;
+    Point ac = c - a;
 
-    double ac_cross_ad = det(c - a, d - a);
-    double ab_cross_ad = det(b - a, d - a);
-    double ca_cross_cb = det(a - c, b - c);
-    double cd_cross_cb = det(d - c, b - c);
+    // Compute cross products
+    double ab_cross_cd = det(ab,cd);
+    double ac_cross_cd = det(ac,cd);
+    double ac_cross_ab = det(ac,ab);
 
-    if ((ac_cross_ad > 0 && ab_cross_ad < 0) || (ac_cross_ad < 0 && ab_cross_ad > 0)) {
-        if ((ca_cross_cb > 0 && cd_cross_cb < 0) || (ca_cross_cb < 0 && cd_cross_cb > 0)) {
-            // Calculate the intersection point
-            double t = ac_cross_ad / (ab_cross_ad - ac_cross_ad);
-            ans = a + t * (b - a);
-            return true;
-        }
+    // Check if the segments are parallel
+    if (ab_cross_cd == 0) {
+        // Segments are parallel or collinear
+        return false;
     }
-    return false;
+
+    // Compute intersection point
+    double t1 = ac_cross_cd / ab_cross_cd;
+    double t2 = ac_cross_ab / ab_cross_cd;
+
+    if (t1 < 0 || t1 > 1 || t2 < 0 || t2 > 1) {
+        // Segments do not intersect
+        return false;
+    }
+
+    ans = a + t1 * ab;
+    return true;
 }
+
+
 
 ////////////////////////////////////////////////////////////////////////////////
 
 bool is_inside(const Polygon &poly, const Point &query)
 {
     // 1. Compute bounding box and set coordinate of a point outside the polygon
-    // TODO
-    Point outside(0, 0);
+    Point outside(6000, 6000);
+
+    //cout << query.real() << " " << query.imag() << endl;
+
+    //cout << "inside func" << endl;
+
     // 2. Cast a ray from the query point to the 'outside' point, count number of intersections
-    // TODO
-    return true;
+    int count = 0;
+    for (int i = 0, j = poly.size(); i < poly.size(); j = i++) {
+        Point intersection;
+        //cout << poly.size() - 15 << endl;
+        if (intersect_segment(poly[i], poly[j], outside, query, intersection)) {
+            if (intersection == query) {
+                return true;
+                cout << "inter == query" << endl;
+            }
+            if ((intersection.real() > query.real()) != (outside.real() > query.real())) {
+                count++;
+                cout << count << endl;
+            }
+        }
+    }
+    //cout << count << endl;
+    if (count % 2 == 1) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -134,6 +170,7 @@ vector<Point> load_xyz(const string &filename)
 void save_xyz(const string &filename, const vector<Point> &points)
 {
     ofstream out(filename);
+    out << points.size() << '\n'; // Write the number of points to the file
 
     for (const auto &p : points)
     {
@@ -142,38 +179,43 @@ void save_xyz(const string &filename, const vector<Point> &points)
     out.close();
 }
 
-Polygon load_obj(const string& filename) {
-    ifstream in(filename);
+
+Polygon load_obj(const std::string& filename) {
+    std::ifstream in(filename);
     if (!in.is_open()) {
-        throw runtime_error("failed to open file " + filename);
+        throw std::runtime_error("failed to open file " + filename);
     }
 
-    Polygon vertices;
-    string line;
-    while (getline(in, line)) {
+    Polygon poly;
+    std::string line;
+    while (std::getline(in, line)) {
         if (line.empty() || line[0] == '#') {
             continue;
         }
 
-        istringstream iss(line);
-        string type;
+        std::istringstream iss(line);
+        std::string type;
         iss >> type;
 
         if (type == "v") {
             double x, y, z;
             iss >> x >> y >> z;
-            vertices.emplace_back(x, y);
+            poly.emplace_back(x, y);
         } else if (type == "f") {
-            string index_str;
+            std::string index_str;
             while (iss >> index_str) {
-                vertices.push_back(vertices[stoi(index_str) - 1]);
+                int index = std::stoi(index_str) - 1;
+                if (index < 0 || index >= poly.size()) {
+                    throw std::runtime_error("invalid vertex index in face");
+                }
             }
+        } else {
+            throw std::runtime_error("invalid file format");
         }
     }
 
-    return vertices;
+    return poly;
 }
-
 
 void save_obj(const string &filename, Polygon &poly)
 {
@@ -210,12 +252,15 @@ int main(int argc, char *argv[])
     //Point in polygon
     Polygon poly = load_obj(poly_path);
     vector<Point> result;
+    cout << points.size() << endl;
     for (size_t i = 0; i < points.size(); ++i)
     {
-        //cout << "inside Point in Polygon loop: " << i << endl;
+        //cout << "loop" << endl;
         if (is_inside(poly, points[i]))
         {
             result.push_back(points[i]);
+            cout << "Added point " << points[i] << endl;
+            
         }
     }
     save_xyz("output.xyz", result);
